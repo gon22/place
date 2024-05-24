1d10644ba441e045fb6aaa0d1a56a4e6d16d8251
gon22 <54576555+gon22@users.noreply.github.com>
2024-05-24 15:56:25 +0900

    Add files via upload

---
+++ naver_place_serch_ranking_v5.py
@@ -0,0 +1,74 @@
+
+import os
+from requests_html import HTMLSession
+import urllib.request
+import json
+import pandas as pd
+import datetime as dt
+import chardet
+import encodings
+import re
+
+# 디렉토리 생성하기
+save_directory = "./naver_place" 
+if not os.path.exists(save_directory):
+    os.makedirs(save_directory)
+
+client_id = os.getenv("NAVER_CLIENT_ID")
+client_secret = os.getenv("NAVER_CLIENT_SECRET")
+
+
+regions = '서울'
+query = ['을지로3가 맛집','을지로3가 와인','을지로3가 위스키', '을지로3가 술집']
+
+def place_top5(client_id, client_secret, query, *regions):
+    regions = list(regions)
+    query = query
+    result_list=[]
+    for region in regions:
+        for q in query:
+            region_query = region + ' ' + q
+            encText = urllib.parse.quote(region_query)
+            url = "https://openapi.naver.com/v1/search/local.json?query=" + encText + "&display=50"
+    
+            request = urllib.request.Request(url)
+            request.add_header("X-Naver-Client-Id", client_id)
+            request.add_header("X-Naver-Client-Secret", client_secret)
+            response = urllib.request.urlopen(request)
+            rescode = response.getcode()
+            
+            if(rescode==200):
+                response_body = response.read()
+                results = json.loads(response_body)['items']
+                
+                for i, result in enumerate(results):
+                    result['rank'] = i+1
+                    result['search'] = q
+                    result['region'] = region
+                    result['title'] = result['title'].replace('<b>','').replace('</b>','')
+                    result['date'] = dt.datetime.now()
+                    result_list.append(result)
+            else:
+                print("Error Code:" + rescode)
+                
+    df = pd.DataFrame(result_list)
+    return df
+
+df = place_top5(client_id, client_secret, query, regions)
+
+df['year'] = df['date'].dt.year
+df['month'] = df['date'].dt.month
+df['day'] = df['date'].dt.day
+df['date_summary'] = df['date'].dt.date
+
+
+# csv에 보여줄 내용
+place = df.copy()
+
+# 파일 경로 설정
+file_path = 'naver_place/keyword_rank_top5test1.csv'
+
+if os.path.exists(file_path):
+    place.to_csv(file_path, mode='a', header=False, index=False, encoding='utf8')
+else:
+    place.to_csv(file_path, mode='w', header=True, index=False, encoding='utf8')
