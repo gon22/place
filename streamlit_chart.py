8acd72e6b7e200fd35cd3e4434de8442f679326c
gon22 <54576555+gon22@users.noreply.github.com>
2024-05-26 21:35:17 +0900

    Add files via upload

--- streamlit_chart.py
+++ streamlit_chart.py
@@ -2,11 +2,28 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
-df = pd.read_csv('naver_place/keyword_rank_top5test1.csv', low_memory=False)
 st.title('을지로3가 맛집')
 # df[df.search == '을지로3가 맛집']
 
-df
-# df.loc['title',df.search == '을지로3가 맛집']
+@st.cache_data
+def long_function():
+    df = pd.read_csv('naver_place/keyword_rank_top5test1.csv', low_memory=False)
+    return df
+
+long_function()
+
+col1, col2, col3 = st.columns(3)
+
+with col1:
+   st.header("A cat")
+   st.image("https://static.streamlit.io/examples/cat.jpg")
+
+with col2:
+   st.header("A dog")
+   st.image("https://static.streamlit.io/examples/dog.jpg")
+
+with col3:
+   st.header("An owl")
+   st.image("https://static.streamlit.io/examples/owl.jpg")
 
 
