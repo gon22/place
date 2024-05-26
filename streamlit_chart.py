0dc2483145592bcd488fdaf7bc115e621e108ce6
gon22 <54576555+gon22@users.noreply.github.com>
2024-05-26 17:26:19 +0900

    Add files via upload

--- streamlit_chart.py
+++ streamlit_chart.py
@@ -6,7 +6,7 @@ df = pd.read_csv('naver_place/keyword_rank_top5test1.csv', low_memory=False)
 st.title('을지로3가 맛집')
 # df[df.search == '을지로3가 맛집']
 
-
-df.loc['title',df.search == '을지로3가 맛집']
+df
+# df.loc['title',df.search == '을지로3가 맛집']
 
 
