411d406f2e424e2684cf8e577fd5ad7612bd8197
gon22 <54576555+gon22@users.noreply.github.com>
2024-05-30 04:04:35 +0900

    Add files via upload

--- naver_place_serch_ranking_v5.py
+++ naver_place_serch_ranking_v5.py
@@ -56,21 +56,10 @@ def place_top5(client_id, client_secret, query, *regions):
 
 df = place_top5(client_id, client_secret, query, regions)
 
-# 연도, 월, 일 추출
 df['year'] = df['date'].dt.year
 df['month'] = df['date'].dt.month
 df['day'] = df['date'].dt.day
-
-# 시간 정보 추출
-df['hour'] = df['date'].dt.hour
-df['minute'] = df['date'].dt.minute
-df['second'] = df['date'].dt.second
-
-# 날짜 요약
 df['date_summary'] = df['date'].dt.date
-
-# 시간 요약 (hh:mm:ss)
-df['time_summary'] = df['date'].dt.time
 
 
 # csv에 보여줄 내용
@@ -83,3 +72,106 @@ if os.path.exists(file_path):
     place.to_csv(file_path, mode='a', header=False, index=False, encoding='utf8')
 else:
     place.to_csv(file_path, mode='w', header=True, index=False, encoding='utf8')
+
+
+
+# 카카오 메시지 알림 - 르템플 순위 변동
+
+from PyKakao import Message
+
+API = Message(service_key = "535beb56f41f8c1d92b5f87300a2fd96")
+
+auth_url = API.get_url_for_generating_code()
+print(auth_url)
+
+url = "https://localhost:5000/?code=UdjtRSsSWGshH1tMhOR3JGAyqZCbJUFQCr35RDkndNZPHXAkFH6H5gAAAAQKKcjZAAABj8V4ux62xj-RG-1vuA"
+
+access_token = API.get_access_token_by_redirected_url(url)
+API.set_access_token(access_token)
+
+# 메시지 유형 - 피드
+message_type = "feed"
+
+# 파라미터
+content = {
+            "title": "오늘의 디저트",
+            "description": "아메리카노, 빵, 케익",
+            "image_url": "https://mud-kage.kakao.com/dn/NTmhS/btqfEUdFAUf/FjKzkZsnoeE4o19klTOVI1/openlink_640x640s.jpg",
+            "image_width": 640,
+            "image_height": 640,
+            "link": {
+                "web_url": "http://www.daum.net",
+                "mobile_web_url": "http://m.daum.net",
+                "android_execution_params": "contentId=100",
+                "ios_execution_params": "contentId=100"
+            }
+        }
+
+item_content = {
+            "profile_text" :"Kakao",
+            "profile_image_url" :"https://mud-kage.kakao.com/dn/Q2iNx/btqgeRgV54P/VLdBs9cvyn8BJXB3o7N8UK/kakaolink40_original.png",
+            "title_image_url" : "https://mud-kage.kakao.com/dn/Q2iNx/btqgeRgV54P/VLdBs9cvyn8BJXB3o7N8UK/kakaolink40_original.png",
+            "title_image_text" :"Cheese cake",
+            "title_image_category" : "Cake",
+            "items" : [
+                {
+                    "item" :"Cake1",
+                    "item_op" : "1000원"
+                },
+                {
+                    "item" :"Cake2",
+                    "item_op" : "2000원"
+                },
+                {
+                    "item" :"Cake3",
+                    "item_op" : "3000원"
+                },
+                {
+                    "item" :"Cake4",
+                    "item_op" : "4000원"
+                },
+                {
+                    "item" :"Cake5",
+                    "item_op" : "5000원"
+                }
+            ],
+            "sum" :"Total",
+            "sum_op" : "15000원"
+        }
+
+social = {
+            "like_count": 100,
+            "comment_count": 200,
+            "shared_count": 300,
+            "view_count": 400,
+            "subscriber_count": 500
+        }
+
+buttons = [
+            {
+                "title": "웹으로 이동",
+                "link": {
+                    "web_url": "http://www.daum.net",
+                    "mobile_web_url": "http://m.daum.net"
+                }
+            },
+            {
+                "title": "앱으로 이동",
+                "link": {
+                    "android_execution_params": "contentId=100",
+                    "ios_execution_params": "contentId=100"
+                }
+            }
+        ]
+
+API.send_message_to_me(
+    message_type=message_type,
+    content=content, 
+    item_content=item_content, 
+    social=social, 
+    buttons=buttons
+    )
+
+
+
+
