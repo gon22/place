8ad468a0e7759bbf382ecb199704393d6cb41208
gon22 <54576555+gon22@users.noreply.github.com>
2024-06-06 20:53:38 +0900

    Add files via upload

--- streamlit_chart.py
+++ streamlit_chart.py
@@ -20,11 +20,11 @@ import re
 
 
 ## 실시간 데이터 보기
-# client_id = os.getenv("NAVER_CLIENT_ID")
-# client_secret = os.getenv("NAVER_CLIENT_SECRET")
+client_id = os.getenv("NAVER_CLIENT_ID")
+client_secret = os.getenv("NAVER_CLIENT_SECRET")
 
-client_id = "K25hzGPrWL8hskBSJWND"
-client_secret = "SSKeYoapXo"
+# client_id = "K25hzGPrWL8hskBSJWND"
+# client_secret = "SSKeYoapXo"
 
 
 regions = '서울'
@@ -142,126 +142,131 @@ df33 = df[df['keyword'] == '을지로3가 위스키']
 df44 = df[df['keyword'] == '을지로3가 술집']
 
 filter = {'을지로3가 맛집': df11, '을지로3가 와인': df22, '을지로3가 위스키': df33, '을지로3가 술집': df44}
-st.header('순위 변동 차트')
-filtered_df = st.selectbox(
-    label="키워드 선택", 
-    help="키워드 선택하여 top5 업체 순위 변동 확인", 
-    options=list(filter.keys())
-)
-
-# 선택된 데이터프레임
-selected_df = filter[filtered_df]
-
-# 타이틀별 색상 할당
-for title in selected_df['title'].unique():
-    if title not in color_map:
-        color_map[title] = next(color_palette)
-
-# Plotly 선 그래프 생성
-fig = px.line(
-    selected_df, 
-    x="kodate", 
-    y="rank", 
-    color="title", 
-    line_group="title", 
-    markers=True,
-    color_discrete_map=color_map  # 색상 맵 적용
-)
-
-# y축을 반대로 설정
-fig.update_yaxes(autorange='reversed')
-
-# 레전드를 위쪽에 배치
-fig.update_layout(
-    xaxis_title="Date", 
-    yaxis_title="Rank", 
-    showlegend=True,
-    legend=dict(
-        orientation="h",
-        yanchor="bottom",
-        y=1.02,
-        xanchor="center",
-        x=0.5
+with st.expander("순위 변동 차트"):
+    filtered_df = st.selectbox(
+        label="키워드 선택", 
+        help="키워드 선택하여 top5 업체 순위 변동 확인", 
+        options=list(filter.keys())
     )
-)
-
-specific_title = "르템플"
-fig.update_traces(
-    line=dict(width=5),  # 원하는 선 두께 지정
-    selector=dict(name=specific_title)  # 특정 타이틀 선택
-)
-
-# 나머지 선 스타일을 대시로 변경
-fig.for_each_trace(
-    lambda trace: trace.update(line=dict(dash='dash')) if trace.name != specific_title else ()
-)
-
-# 스트림릿에 Plotly 그래프 표시
-st.plotly_chart(fig, use_container_width=True)
+    
+    # 선택된 데이터프레임
+    selected_df = filter[filtered_df]
+    
+    # 타이틀별 색상 할당
+    for title in selected_df['title'].unique():
+        if title not in color_map:
+            color_map[title] = next(color_palette)
+    
+    # Plotly 선 그래프 생성
+    fig = px.line(
+        selected_df, 
+        x="kodate", 
+        y="rank", 
+        color="title", 
+        line_group="title", 
+        markers=True,
+        color_discrete_map=color_map  # 색상 맵 적용
+    )
+    
+    # y축을 반대로 설정
+    fig.update_yaxes(autorange='reversed')
+    
+    # 레전드를 위쪽에 배치
+    fig.update_layout(
+        xaxis_title="Date", 
+        yaxis_title="Rank", 
+        showlegend=True,
+        legend=dict(
+            orientation="h",
+            yanchor="bottom",
+            y=1.02,
+            xanchor="center",
+            x=0.5
+        )
+    )
+    
+    specific_title = "르템플"
+    fig.update_traces(
+        line=dict(width=5),  # 원하는 선 두께 지정
+        selector=dict(name=specific_title)  # 특정 타이틀 선택
+    )
+    
+    # 나머지 선 스타일을 대시로 변경
+    fig.for_each_trace(
+        lambda trace: trace.update(line=dict(dash='dash')) if trace.name != specific_title else ()
+    )
+    
+    # 스트림릿에 Plotly 그래프 표시
+    st.plotly_chart(fig, use_container_width=True)
 
 
 ################################################################
 
 
-st.header(f'플레이스 TOP5')
 
-col1, col2= st.columns(2)
+with st.expander("플레이스 TOP5"):
 
-with col1:
-   # 날짜 선택 필터
-    option = st.date_input(
-        "날짜", 
-        help='2024-05-26 이후부터 가능',
-        min_value=datetime(2024, 5, 26),
-        value=datetime(today.year, today.month, today.day)  # 초기값 설정
-    )
-with col2:
-    # 오전/오후 선택 필터
-    apm = ['오전', '오후']
-    option_apm = st.selectbox(
-        label="시점", 
-        help="오전 (pm01), 오후 (pm07)",
-        options=apm
-    )
-
-
-# 키워드에 따른 데이터 필터링 함수
-def filter_data(keyword, date, period):
-    return df.loc[(df['keyword'] == keyword) & (df['date_only'] == date) & (df['period'] == period), ['rank', 'title']].rename(columns={'title': keyword})
-
-# 키워드별 데이터프레임 생성 및 열 이름 변경
-df1 = filter_data('을지로3가 맛집', option, option_apm)
-df2 = filter_data('을지로3가 와인', option, option_apm)
-df3 = filter_data('을지로3가 위스키', option, option_apm)
-df4 = filter_data('을지로3가 술집', option, option_apm)
-
-# 'rank'를 기준으로 병합
-merged_df = df1.merge(df2, on='rank', how='outer')\
-               .merge(df3, on='rank', how='outer')\
-               .merge(df4, on='rank', how='outer')
-
-# 중복된 행 제거
-merged_df = merged_df.drop_duplicates(subset='rank')
-
-# 'rank' 기준으로 정렬하고 인덱스로 설정
-merged_df = merged_df.sort_values(by='rank').set_index('rank')
-
-# 정렬된 데이터프레임 출력
-st.dataframe(merged_df)
-
-###############################################
-
-# 키워드에 따른 데이터 필터링 함수
-def clips_data(keyword, df):
-    if option and option_apm:
-        clip = df[df[keyword] == '르템플']['rank'].values.tolist()[0]
-    return st.write(f'{keyword} : {clip}위')
-try:
-    clips_data('을지로3가 맛집', df1)
-    clips_data('을지로3가 와인', df2)
-    clips_data('을지로3가 위스키', df3)
-    clips_data('을지로3가 술집', df4)
-except:
-    st.write('')
+    col1, col2= st.columns(2)
+    
+    with col1:
+       # 날짜 선택 필터
+        option = st.date_input(
+            "날짜", 
+            help='2024-05-26 이후부터 가능',
+            min_value=datetime(2024, 5, 26),
+            value=datetime(today.year, today.month, today.day)  # 초기값 설정
+        )
+    with col2:
+        # 오전/오후 선택 필터
+        apm = ['오전', '오후']
+        option_apm = st.selectbox(
+            label="시점", 
+            help="오전 (pm01), 오후 (pm07)",
+            options=apm
+        )
+    
+    
+    # 키워드에 따른 데이터 필터링 함수
+    def filter_data(keyword, date, period):
+        return df.loc[(df['keyword'] == keyword) & (df['date_only'] == date) & (df['period'] == period), ['rank', 'title']].rename(columns={'title': keyword})
+    
+    # 키워드별 데이터프레임 생성 및 열 이름 변경
+    df1 = filter_data('을지로3가 맛집', option, option_apm)
+    df2 = filter_data('을지로3가 와인', option, option_apm)
+    df3 = filter_data('을지로3가 위스키', option, option_apm)
+    df4 = filter_data('을지로3가 술집', option, option_apm)
+    
+    # 'rank'를 기준으로 병합
+    merged_df = df1.merge(df2, on='rank', how='outer')\
+                   .merge(df3, on='rank', how='outer')\
+                   .merge(df4, on='rank', how='outer')
+    
+    # 중복된 행 제거
+    merged_df = merged_df.drop_duplicates(subset='rank')
+    
+    # 'rank' 기준으로 정렬하고 인덱스로 설정
+    merged_df = merged_df.sort_values(by='rank').set_index('rank')
+    
+    # 정렬된 데이터프레임 출력
+    def highlight_color(val):
+        color = 'yellow' if val == '르템플' else 'white'
+        return f'color: {color}'
+    styled_df = merged_df.style.applymap(highlight_color)
+    st.dataframe(styled_df)
+    
+    ###############################################
+    
+    # 키워드에 따른 데이터 필터링 함수
+    def clips_data(keyword, df):
+        if option and option_apm:
+            clip = df[df[keyword] == '르템플']['rank'].values.tolist()[0]
+        return st.write(f'{keyword} : {clip}위')
+    try:
+        clips_data('을지로3가 맛집', df1)
+        clips_data('을지로3가 와인', df2)
+        clips_data('을지로3가 위스키', df3)
+        clips_data('을지로3가 술집', df4)
+    except:
+        st.write('')
 
 
